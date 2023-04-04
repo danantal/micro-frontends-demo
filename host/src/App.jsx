077386cab1f9628847de6caac9d01cebcad7ef00
Dan Antal <antaldan95@gmail.com>
2023-04-04 14:34:28 +0300

    Add lazy load and fallback

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -3,12 +3,41 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-import { Counter } from "remote/Counter";
+const Counter = React.lazy(() =>
+  import("remote/Counter").then((module) => ({ default: module.Counter }))
+);
+
+class ModuleErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log("ModuleErrorBoundary caught an error", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
 
 const App = () => (
   <div className="container">
     <div>Name: host</div>
-    <Counter />
+    <ModuleErrorBoundary fallback={"Counter failed to load"}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Counter />
+      </Suspense>
+    </ModuleErrorBoundary>
   </div>
 );
 
